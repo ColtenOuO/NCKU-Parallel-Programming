c6972055b290a862c0a78ee104a9701b4b40e2f2
Colten <70793703+ColtenOuO@users.noreply.github.com>
2024-10-26 05:42:34 +0800

    Update hw2-2_tle.cpp

--- hw2-2_tle.cpp
+++ hw2-2_tle.cpp
@@ -3,91 +3,73 @@
 
 using namespace std;
 
-vector<pair<int, int>> e[50001];
+vector<pair<int,int>> e[50001];
 char message[100] = {};
-
-struct MinLoc {
-    int value;
-    int index;
-};
-
 int main(int argc, char *argv[]) {
+   std::ios_base::sync_with_stdio(false);
+   std::cin.tie(0);
     MPI_Init(&argc, &argv);
-
+    
     int world_size, world_rank;
     MPI_Comm_size(MPI_COMM_WORLD, &world_size);
     MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
-    
-    if (world_rank == 0) {
-        string file_name;
-        cin >> file_name;
+    std::string file_name;
+    if( world_rank == 0 ) {
+        std::cin >> file_name;
         strcpy(message, file_name.c_str());
         message[sizeof(message) - 1] = '\0';
     }
 
     MPI_Bcast(message, 100, MPI_CHAR, 0, MPI_COMM_WORLD);
-
-    ifstream input_file(message);
+    std::ifstream input_file(message);
     int n;
     input_file >> n;
     int u;
 
-    int block = (50000 + world_size - 1) / world_size;
+    int block = 50000 / world_size;
     int cnt = 0;
-    while (input_file >> u) {
-        int v, w;
-        input_file >> v >> w;
-        if (cnt >= world_rank * block && cnt < (world_rank + 1) * block) {
-            e[u].push_back({v, w});
+    while( input_file >> u ) {
+        int v;
+        input_file >> v;
+        int w;
+        input_file >> w;
+        if( cnt < ( world_rank + 1 ) * block && cnt >= world_rank * block ) {
+            e[u].push_back({v,w});
+            e[v].push_back({u,w});
         }
+
         cnt++;
     }
     input_file.close();
-
-    vector<int> dis(n, (int)1e9), ok(n, 0);
+    vector<int> dis(n,(int)1e9),ok(n,0);
     dis[0] = 0;
-    ok[0] = 1;
+    ok[0] = 0;
     int v = 0;
-
-    for (int i = 0; i < n - 1; i++) {
-        MinLoc local_min = {(int)1e9, -1};
-        for (auto j : e[v]) {
-            dis[j.first] = min(dis[j.first], dis[v] + j.second);
-            if (!ok[j.first] && dis[j.first] < local_min.value) {
-                local_min.value = dis[j.first];
-                local_min.index = j.first;
-            }
-        }
-
-        int ll = world_rank * (n / world_size);
-        int rr = (world_rank + 1) * (n / world_size) - 1;
-        if (world_rank == world_size - 1) rr = n - 1;
-
-        for (int j = 0; j < n; j++) {
-            if (!ok[j] && dis[j] < local_min.value) {
-                local_min.value = dis[j];
-                local_min.index = j;
-            }
-        }
+    for(int i = 0; i < n - 1; i++) {
+        MPI_Allreduce(MPI_IN_PLACE, dis.data(), n, MPI_INT, MPI_MIN, MPI_COMM_WORLD);
         
-        MinLoc global_min = {(int)1e9, -1};
-        MPI_Reduce(&local_min, &global_min, 1, MPI_2INT, MPI_MINLOC, 0, MPI_COMM_WORLD);
-        MPI_Bcast(&global_min, 1, MPI_2INT, 0, MPI_COMM_WORLD);
-        
-
-        if (global_min.index == -1) break;
-
-        ok[global_min.index] = 1;
-        v = global_min.index;
-        dis[v] = global_min.value;
-    }
-    
-
-    if (world_rank == 0) {
-        string ans;
+        int mn = 1e9, pos = -1;
         for (int i = 0; i < n; i++) {
-            ans += to_string(dis[i]) + " ";
+            if (ok[i] == 0 && mn > dis[i]) {
+                mn = dis[i];
+                pos = i;
+            }
         }
+        ok[pos] = 1;
+        v = pos;
+        
+        for (auto j : e[v]) {
+            dis[j.first] = min(dis[j.first], dis[v] + j.second); 
+        }
+    }
+
+    if( world_rank == 0 ) {
+        string ans = "";
+        for(int i=0;i<n;i++) {
+            ans += to_string(dis[i]);
+            ans += " ";
+        }
+
         cout << ans;
     }
 
